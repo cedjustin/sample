5985465393e43ddadde3f86fb1b2e3e546e0dbfc
Cedjustin <cedro265@gmail.com>
2020-10-21 08:32:50 +0100

    initial commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+
+    before_action :set_task, only: [:edit, :update, :destroy]
+
+    def index
+        @tasks = Task.all
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            redirect_to tasks_path
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @task.update(task_params)
+            redirect_to tasks_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @task.destroy
+        redirect_to tasks_path
+    end
+
+    def confirm
+        @task = Task.new(task_params)
+    end
+
+    private
+
+    def set_task
+        @task = Task.find(params[:id])
+    end
+
+    def task_params
+        params.require(:task).permit(:content)
+    end
+
+end
